e38ea534b901cc720d358196ac271887f03807b2
hk <hk@sockenbuegeln.de>
2016-03-13 17:20:05 +0100

    forgot this file

--- util.h
+++ util.h
@@ -2,7 +2,7 @@
 #define __UTIL_H__
 
 error_e toFpn( uint16_t pre, uint16_t post, fpn_t out );
-void toDFpn( uint16_t in, dfpn_t *out );
+error_e toDFpn( uint16_t pre, uint16_t post, fpn_t out );
 int8_t isLarger( fpn_t a, fpn_t b );
 
 #else
